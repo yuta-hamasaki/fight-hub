a887e6dec5d30c828c12ba39f2b26026737030a9
Yuta Hamasaki <81616117+yuta-hamasaki@users.noreply.github.com>
2026-09-07 08:44:01 -0700

    Add dummy trainer seed data

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "node prisma/seed.mjs",
   },
   datasource: {
     url: env("DATABASE_URL"),
